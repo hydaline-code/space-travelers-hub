fa0f5818c1378e41128f7aa6534dcebb1d927e7b
brighton0102 <mtandibrighton0102@gmail.com>
2023-10-11 12:12:33 +0200

    Fetch and display rocket data

--- src/component/Rockets.js
+++ src/component/Rockets.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './styles/Rockets.css';
 
-const Rockets = () => (
-  <div>
-    <h1>Hello from Rockets</h1>
-  </div>
-);
+function Rockets() {
+  const rockets = useSelector((state) => state.rocket);
+
+  return (
+    <div>
+      <div className="rocket-container">
+        {rockets.map((rocket) => (
+          <div key={rocket.id} className="rocket-elements">
+            <img src={rocket.flickr_images[0]} alt={rocket.name} className="rocket-image" />
+            <div className="rocket-details">
+              <h3>{rocket.name}</h3>
+              <p>
+                Type:
+                {rocket.type}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Rockets;
