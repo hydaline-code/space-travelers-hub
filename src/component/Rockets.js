1a388458e12198950a455df1eaf2216eb08d1c02
brighton0102 <mtandibrighton0102@gmail.com>
2023-10-11 16:04:45 +0200

    rocket-booking

--- src/component/Rockets.js
+++ src/component/Rockets.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { reserveRocket } from '../redux/slice/rockets/RocketSlice';
 import './styles/Rockets.css';
 
 function Rockets() {
   const rockets = useSelector((state) => state.rocket);
+  const dispatch = useDispatch();
+
+  const handleReserveRocket = (rocketId) => {
+    dispatch(reserveRocket(rocketId));
+  };
 
   return (
     <div>
@@ -13,10 +19,22 @@ function Rockets() {
             <img src={rocket.flickr_images[0]} alt={rocket.name} className="rocket-image" />
             <div className="rocket-details">
               <h3>{rocket.name}</h3>
-              <p>
-                Type:
-                {rocket.type}
-              </p>
+              {rocket.reserved ? (
+                <div>
+                  <p className="reserved-description">Reserved</p>
+                  <p>{ rocket.description }</p>
+                </div>
+              ) : (
+                <p>{ rocket.description }</p>
+              )}
+              <button
+                type="button"
+                className="reserve-btn"
+                onClick={() => handleReserveRocket(rocket.id)}
+                disabled={rocket.reserved}
+              >
+                {rocket.reserved ? 'Cacel Reservation' : 'Reserve Rocket'}
+              </button>
             </div>
           </div>
         ))}
