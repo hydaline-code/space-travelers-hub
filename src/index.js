ae04c90ae3ca1789119ae4a7ca3230c4195f0dca
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-10-11 11:19:54 +0300

    Fetch data

--- src/index.js
+++ src/index.js
@@ -13,3 +13,19 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from './redux/store';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
