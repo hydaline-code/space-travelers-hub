2ed26539e9ce10d49dd5083db090f7d16a566a6a
hydaline-code <djougancharlen@.com>
2023-10-12 05:34:54 +0200

    Add button functionality for join or leave mission

--- src/component/Missions.js
+++ src/component/Missions.js
@@ -1,51 +1,30 @@
 
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissions } from '../redux/slice/MissionsSlice';
+import { fetchMissions, joinMission, leaveMission } from '../redux/slice/MissionsSlice';
 import './styles/Missions.css';
 
-const Missions = () => {
+const Missions = ({ mission }) => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.missions.missions);
+
+  const handleJoinOrLeaveMission = (missionId, reserved) => {
+    if (reserved) {
+      dispatch(leaveMission(missionId));
+    } else {
+      dispatch(joinMission(missionId));
+    }
+  };
 
   useEffect(() => {
     dispatch(fetchMissions());
   }, [dispatch]);
 
   console.log('Missions in component:', missions);
-
-  // return (
-  //   <div className='missions-container'>
-  //      <h2>Missions</h2>
-  //     <ul>
-  //       <li className='column-title'>
-  //       <div>
-  //             <div>mission</div>
-  //             <div>Description</div>
-  //             <div>Status</div>
-  //         </div>
-  //       </li>
-
-  //    <li>
-  //     {Array.isArray(missions) ? (
-  //       missions.map((mission) => (
-  //         <div className='row-description' key={mission.mission_id}>
-  //          <div><h3>{mission.mission_name}</h3> </div> 
-  //          <div> <p>{mission.description}</p> </div>
-  //         </div>
-  //       ))
-  //     ) : (
-  //       <h4 className='no-fetch'>No missions data available.</h4>
-  //     )}
-  //     </li>
-  //     </ul>
-  //   </div>   
-  // );
-
   return (
     <div className='missions-container'>
       <h2>Missions</h2>
-      <ul>
+      <ul className='mission-list'>
         <li className='column-title'>
           <div className='grid-container'>
             <div>Mission</div>
@@ -62,8 +41,10 @@ const Missions = () => {
                 <div><h3>{mission.mission_name}</h3></div>
                 <div className='description'><p className='desc'>{mission.description}</p></div>
                 <div className='status'>{mission.status}
-                <button>one</button></div>
-                <div className='second-btn'><button>two</button></div>
+                <button>Not a member</button></div>
+                <div className='second-btn'><button onClick={() => handleJoinOrLeaveMission(mission.mission_id, mission.reserved)}>
+                  {mission.reserved ? 'Leave Mission' : 'Join Mission'}
+                </button></div>
               </div>
             </li>
           ))
@@ -76,4 +57,9 @@ const Missions = () => {
 
 };
 export default Missions;
+
+
+
+
+
 
--- src/redux/slice/MissionsSlice.js
+++ src/redux/slice/MissionsSlice.js
@@ -1,6 +1,29 @@
 
 import { createSlice } from '@reduxjs/toolkit';
 
+export const fetchMissions = () => async (dispatch) => {
+  try {
+    dispatch(setLoading(true));
+
+    // Fetch missions data
+    const response = await fetch('https://api.spacexdata.com/v3/missions');
+
+    if (!response.ok) {
+      throw new Error('Network response failed');
+    }
+
+    const data = await response.json();
+    // Convert the response object into an array
+    const missionData = Object.values(data);
+    // Dispatch the action to set missions data
+    dispatch(setMissions(missionData));
+  } catch (error) {
+    
+    dispatch(setError(error.message));
+  }
+};
+
+
 const initialState = {
   missions: [],
   isLoading: false,
@@ -23,32 +46,35 @@ export const missionsSlice = createSlice({
       state.error = action.payload;
       state.isLoading = false;
     },
+    joinMission: (state, action) => {
+      const missionId = action.payload;
+      const missionToUpdate = state.missions.find((mission) => mission.mission_id === missionId);
+      if (missionToUpdate) {
+        missionToUpdate.reserved = true;
+      }
+    },
+    leaveMission: (state, action) => {
+      const missionId = action.payload;
+      const missionToUpdate = state.missions.find((mission) => mission.mission_id === missionId);
+      if (missionToUpdate) {
+        missionToUpdate.reserved = false;
+      }
+    }    
+    
   },
+  // extraReducers: (builder) => {
+  //   builder
+  //     .addCase(joinMission, (state, action) => {
+  //       const missionId = action.payload;
+  //       // Update the state based on the missionId for joining a mission
+  //     })
+  //     .addCase(leaveMission, (state, action) => {
+  //       const missionId = action.payload;
+  //       // Update the state based on the missionId for leaving a mission
+  //     });
+  // },
 });
 
-export const { setMissions, setLoading, setError } = missionsSlice.actions;
-
-export const fetchMissions = () => async (dispatch) => {
-  try {
-    dispatch(setLoading(true));
-
-    // Fetch missions data
-    const response = await fetch('https://api.spacexdata.com/v3/missions');
-
-    if (!response.ok) {
-      throw new Error('Network response failed');
-    }
-
-    const data = await response.json();
-
-    // Convert the response object into an array
-    const missionData = Object.values(data);
-    // Dispatch the action to set missions data
-    dispatch(setMissions(missionData));
-  } catch (error) {
-    
-    dispatch(setError(error.message));
-  }
-};
+export const { setMissions, setLoading, setError, joinMission, leaveMission } = missionsSlice.actions;
 
 export default missionsSlice.reducer;
