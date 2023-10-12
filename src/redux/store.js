af74edb36db5fab9e755aabfe620dd0c0396a7b4
hydaline-code <107451859+hydaline-code@users.noreply.github.com>
2023-10-12 15:36:50 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
-
 import logger from 'redux-logger';
 import { configureStore } from '@reduxjs/toolkit';
 import dragonReducer from './slice/dragons/DragonSlice';
 import rocketReducer from './slice/rockets/RocketSlice';
 import missionReducer from './slice/missions/MissionsSlice';
-
 const store = configureStore({
   reducer: {
     dragon: dragonReducer,
