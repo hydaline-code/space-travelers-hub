7be3f3a6bad78f08c3991eddc8599b8e5d7a8cee
hydaline-code <djougancharlen@.com>
2023-10-12 16:49:07 +0200

    Fix issues of app not runing locally

--- src/App.js
+++ src/App.js
@@ -6,29 +6,30 @@ import Missions from './component/Missions';
 import Profile from './component/Profile';
 import Navigation from './component/Navigation';
 import { setRockets } from './redux/slice/rockets/RocketSlice';
+import { useDispatch } from 'react-redux';
 
 function App() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    async function fetchRockets() {
-      try {
-        const response = await fetch('https://api.spacexdata.com/v3/rockets');
-        const data = await response.json();
-        const rocketsData = data.map((rocket) => ({
-          id: rocket.rocket_id,
-          name: rocket.rocket_name,
-          type: rocket.rocket_type,
-          flickr_images: rocket.flickr_images,
-        }));
-        dispatch(setRockets(rocketsData));
-      } catch (error) {
-        console.error('Error fetching rockets data:', error);
-      }
-    }
+  // useEffect(() => {
+  //   async function fetchRockets() {
+  //     try {
+  //       const response = await fetch('https://api.spacexdata.com/v3/rockets');
+  //       const data = await response.json();
+  //       const rocketsData = data.map((rocket) => ({
+  //         id: rocket.rocket_id,
+  //         name: rocket.rocket_name,
+  //         type: rocket.rocket_type,
+  //         flickr_images: rocket.flickr_images,
+  //       }));
+  //       dispatch(setRockets(rocketsData));
+  //     } catch (error) {
+  //       console.error('Error fetching rockets data:', error);
+  //     }
+  //   }
 
-    fetchRockets();
-  }, [dispatch]);
+  //   fetchRockets();
+  // }, [dispatch]);
 
   return (
     <Router>
