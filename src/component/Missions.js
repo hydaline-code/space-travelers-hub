4b13dc4c3033f32e052c3acfe2db20897943ec3f
hydaline-code <djougancharlen@.com>
2023-10-13 12:37:49 +0200

    Update code for testing

--- src/component/Missions.js
+++ src/component/Missions.js
@@ -1,13 +1,88 @@
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import { fetchMissions, joinMission, leaveMission } from '../redux/slice/missions/MissionsSlice';
+// import './styles/Missions.css';
+
+// const Missions = () => {
+//   const dispatch = useDispatch();
+//   const missions = useSelector((state) => state.missions.missions);
+
+//   const handleMissionStatus = (missionId, reserved) => {
+//     if (reserved) {
+//       dispatch(leaveMission(missionId));
+//     } else {
+//       dispatch(joinMission(missionId));
+//     }
+//   };
+
+//   useEffect(() => {
+//     dispatch(fetchMissions());
+//   }, [dispatch]);
+
+//   return (
+//     <div className="missions-container">
+//       <h2>Missions</h2>
+//       <table className="mission-list">
+//         <thead>
+//           <tr className="column-title border">
+//             <th>Mission</th>
+//             <th>Description</th>
+//             <th>Status</th>
+//             <th>Button</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {Array.isArray(missions) ? (
+//             missions.map((mission) => (
+//               <tr className="row-description border" key={mission.mission_id}>
+//                 <td><h5>{mission.mission_name}</h5></td>
+//                 <td className="description"><p>{mission.description}</p></td>
+//                 <td className="status">
+//                   {mission.reserved ? <button type="button" className="badge">ACTIVE MEMBER</button> : <button type="button" className="not-member">NOT A MEMBER</button>}
+//                 </td>
+//                 <td className="second-btn">
+//                   <button
+//                     type="button"
+//                     onClick={() => handleMissionStatus(mission.mission_id, mission.reserved)}
+//                     className={mission.reserved ? 'leave-button' : 'join-button'}
+//                   >
+//                     {mission.reserved ? 'Leave Mission' : 'Join Mission'}
+//                   </button>
+//                 </td>
+//               </tr>
+//             ))
+//           ) : (
+//             <tr className="row-description border">
+//               <td colSpan="4">
+//                 <h4 className="no-fetch">No missions data available.</h4>
+//               </td>
+//             </tr>
+//           )}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// Missions.defaultProps = {
+//   mission: {
+//     mission_name: PropTypes.string.isRequired,
+//   },
+// };
+
+// export default Missions;
+
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { fetchMissions, joinMission, leaveMission } from '../redux/slice/missions/MissionsSlice';
 import './styles/Missions.css';
 
 const Missions = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.missions.missions);
-
+  
   const handleMissionStatus = (missionId, reserved) => {
     if (reserved) {
       dispatch(leaveMission(missionId));
@@ -33,7 +108,7 @@ const Missions = () => {
           </tr>
         </thead>
         <tbody>
-          {Array.isArray(missions) ? (
+          {Array.isArray(missions) && missions.length > 0 ? (
             missions.map((mission) => (
               <tr className="row-description border" key={mission.mission_id}>
                 <td><h5>{mission.mission_name}</h5></td>
@@ -65,10 +140,14 @@ const Missions = () => {
   );
 };
 
-Missions.defaultProps = {
-  mission: {
-    mission_name: PropTypes.string.isRequired,
-  },
-};
+// Missions.propTypes = {
+//   // If you pass mission as a prop, you need to validate it. Otherwise, you can remove this.
+//   mission: PropTypes.shape({
+//     mission_name: PropTypes.string.isRequired,
+//     description: PropTypes.string,
+//     reserved: PropTypes.bool,
+//     mission_id: PropTypes.number,
+//   }),
+// };
 
 export default Missions;
