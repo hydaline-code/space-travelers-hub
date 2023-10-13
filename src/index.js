20ece8c5f481edd15f6b230ce7b5b535462f930e
hydaline-code <djougancharlen@.com>
2023-10-13 12:23:07 +0200

    Remove commented code

--- src/index.js
+++ src/index.js
@@ -15,18 +15,3 @@ root.render(
   </React.StrictMode>,
 );
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import store from './redux/store';
-// import './index.css';
-// import App from './App';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// );
--- src/redux/slice/rockets/RocketSlice.js
+++ src/redux/slice/rockets/RocketSlice.js
@@ -1,28 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-
-// const dispatch = useDispatch();
-
-// useEffect(() => {
-//   async function fetchRockets() {
-//     try {
-//       const response = await fetch('https://api.spacexdata.com/v3/rockets');
-//       const data = await response.json();
-//       const rocketsData = data.map((rocket) => ({
-//         id: rocket.rocket_id,
-//         name: rocket.rocket_name,
-//         type: rocket.rocket_type,
-//         flickr_images: rocket.flickr_images,
-//       }));
-//       dispatch(setRockets(rocketsData));
-//     } catch (error) {
-//       console.error('Error fetching rockets data:', error);
-//     }
-//   }
-
-//   fetchRockets();
-// }, [dispatch]);
-
 const rocketSlice = createSlice({
   name: 'rocket',
   initialState: [],
